8733acb0ac7358e9aed65fd3b226b7f09126cf67
IMMZY <imeessienime@gmail.com>
2024-08-28 12:57:43 -0700

    0-island_perimeter task_0

--- 0-main.py
+++ 0-main.py
@@ -2,15 +2,14 @@
 """
 0-main
 """
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
+island_perimeter = __import__('0-island_perimeter').island_perimeter
 
 if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
